d5be9f71d8d06160ea0d74c1c512b179594db1dd
Ines Angelica Thompson <i.thompson89@outlook.com>
2019-07-13 01:07:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,3 @@ def reverse_each_word(string)
   return string.split(" ").collect(&:reverse!).join(" ")
 # return array.each("Hello there, and how are you?" )
 end 
-
-def reverse_each_word(array)
-  return array.collect("Hello there, and how are you?")
-end
