942685e47e675930e31edf971930767becf45669
Ines Angelica Thompson <i.thompson89@outlook.com>
2019-07-13 00:57:58 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,7 @@
+def reverse_each_word (string)
+return array.each("Hello there, and how are you?" )
+end 
+
+def reverse_each_word(array)
+  return array.collect("Hello there, and how are you?")
+end
